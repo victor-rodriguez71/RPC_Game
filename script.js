0262208b75bad4fa804688d494ce766b00b7a948
Victor Rodriguez <188802056+victor-rodriguez71@users.noreply.github.com>
2025-04-29 23:46:14 -0600

    Updated javascript with code for game. Need to add code to play 5 rounds.

---
+++ script.js
@@ -0,0 +1,60 @@
+// Pseudo Code 
+// Have the computer select a random choice: Rock, Paper, or Scissor
+// Have the player select a random choice: Rock, Paper, or Scissor
+// Compare the two: Paper beats Rock 
+//                  Rock beats Scissors
+//                  Scissors beats Paper
+
+function getComputerChoice() {
+    let answer = Math.floor(Math.random() * 3) + 1;
+
+    console.log(answer);
+    return answer;
+
+}
+
+function getHumanChoice() {
+    let answer = prompt("Enter 'Rock', 'Paper', or 'Scissors'");
+    answer = answer.toUpperCase();
+    let humanChoice = 0;
+
+    if (answer === "ROCK") {
+        humanChoice = 1;
+    } else if ( answer === "PAPER") {
+        humanChoice = 2;
+    } else if (answer === "SCISSORS") {
+        humanChoice = 3;
+    } else {
+        alert("Invalid input! Please enter Rock, Paper, or Scissors.")
+        return getHumanChoice(); 
+    }
+    
+    console.log(humanChoice);
+    return humanChoice
+
+}
+
+let humanScore = 0;
+let computerScore = 0;
+let round = 0;
+
+function playRound(computerChoice, humanChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("Try Again!");
+    } else if (humanChoice < computerChoice) {
+        console.log("You lose!");
+        computerScore++;
+        round++;
+    } else {
+        console.log("You win!");
+        humanScore++;
+    }
+
+    round++;
+    console.log(`Human Score: ${humanScore} | Computer Score: ${computerScore} | Round: ${round}`);
+}
+
+let computerChoice = getComputerChoice();
+let humanChoice = getHumanChoice();
+
+playRound(computerChoice, humanChoice);
